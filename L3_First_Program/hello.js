b1c07ea7638a3bf799b03c21270ab61341236186
Arbaz Ansari <mail.arbazansari@gmail.com>
2024-09-27 02:20:23 +0530

    lec 3 is completed basic of nodejs

--- L3_First_Program/hello.js
+++ L3_First_Program/hello.js
@@ -1,1 +1,8 @@
 console.log("Hi! Arbaz, This is javascript");
+console.log(
+    "Welcome to NodeJs Mastering and welcome to the world of programming"
+);
+
+// console.log(alert("hello")); // This will display an alert box with the message "hello" in it. but why -> because console.log() is used to print the message to the console.
+
+// console.log(window); // window is not defined -> it is a global object in the browser environment. It is not available in Node.js environment.
