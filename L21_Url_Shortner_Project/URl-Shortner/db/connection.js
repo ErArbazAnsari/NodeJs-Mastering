53f6922ec52d3eb404c749c476c03a9c75a4aa0d
Arbaz Ansari <mail.arbazansari@gmail.com>
2024-11-12 23:45:48 +0530

    update

--- L21_Url_Shortner_Project/URl-Shortner/db/connection.js
+++
@@ -1,9 +0,0 @@
-const mongoose = require("mongoose");
-
-const connectDB = (url) =>
-    mongoose
-        .connect(url)
-        .then(console.log("Database is connected"))
-        .catch((error) => console.log(error));
-
-module.exports = connectDB;
--- L21_Url_Shortner_Project/URl-Shortner/routes/url.js
+++
@@ -1,8 +0,0 @@
-const express = require("express");
-const router = express.Router();
-const { generateNewShortURL, getAnalytics } = require("../controllers/url");
-
-router.post("/", generateNewShortURL);
-router.get("/analytics/:id", getAnalytics);
-
-module.exports = router;
